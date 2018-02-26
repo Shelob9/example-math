cdc8c1b7eec94ee3a223ef2dad3b7a78a36da6c3
Josh Pollock <josh@calderawp.com>
2018-02-26 18:08:43 -0500

    Use composer for autoloader

--- index.php
+++ index.php
@@ -1,39 +1,5 @@
 <?php
-
-//autoloader
-//@see https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-4-autoloader-examples.md
-spl_autoload_register(function ($class) {
-
-    // project-specific namespace prefix
-    $prefix = 'ExampleMath\\';
-
-    // base directory for the namespace prefix
-    $base_dir = __DIR__ . '/src/';
-
-    // does the class use the namespace prefix?
-    $len = strlen($prefix);
-    if (strncmp($prefix, $class, $len) !== 0) {
-        // no, move to the next registered autoloader
-        return;
-    }
-
-    // get the relative class name
-    $relative_class = substr($class, $len);
-
-    // replace the namespace prefix with the base directory, replace namespace
-    // separators with directory separators in the relative class name, append
-    // with .php
-    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
-
-    // if the file exists, require it
-    if (file_exists($file)) {
-        require $file;
-    }
-});
-
-
-
-
+include_once __DIR__ . '/vendor/autoload.php';
 
 /**
  * Factory for objects implementing the OperatesOnNumber interface.
